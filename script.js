9b65fc02419c22cbb53f8058aec05c17f27070ff
David Biolchini <biolchinid@gmail.com>
2020-05-08 17:35:23 -0300

    added rainbow mode

--- script.js
+++ script.js
@@ -3,7 +3,10 @@
 const gridContainer = document.querySelector(".gridContainer");
 const newGridButton = document.querySelector("#newGrid");
 const pixelSizeButton = document.querySelector("#pixelSize");
+const rainbowModeButton = document.querySelector('#rainbowMode');
+const blackModeButton = document.querySelector("#blackMode")
 let columnsArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+let colorsArray = ['#9400D3','#4B0082', '#0000FF', '#00FF00', '#FFFF00','#FF7F00', '#FF0000'];
 
 
 
@@ -35,13 +38,41 @@ function blackChange () {
     boxes.forEach((div) => {
         div.addEventListener("mouseover",(e) => {
             div.classList.add("black");
+            div.classList.remove("white");
+            div.classList.remove("rainbow");
             });
     });
 }
 
+function rainbowModeChange () {
+    let rainbowMode ; 
+        boxes.forEach((div) => {
+            div.classList.add("rainbow");
+            rainbowMode = document.querySelectorAll(".rainbow"); 
+            let randomColor = colorsArray[Math.floor(Math.random()*7)];
+            div.style.cssText = `background-color: ${randomColor};`;
+            div.classList.add("white");
+    });
+    
+    
+    rainbowMode.forEach((div) =>   {    
+        div.addEventListener("mouseover", (e) => {
+            div.classList.remove("white");
+            div.classList.remove("black");
+            
+            console.log("ok");
+        });
+        
+    });
+    
+}
+
 function newGridChange() {
     boxes.forEach((div) => {
-        div.classList.remove("black")       
+        div.classList.remove("black");
+        div.classList.remove("rainbow");       
+        div.classList.remove("shadow");
+        div.classList.add("white");
     });
 
 }
@@ -83,6 +114,7 @@ function setPixelSize () {
      columnsArray.forEach(createRows);
      boxes = document.querySelectorAll(".box");
      
+     
      blackChange();
  }
 
@@ -93,6 +125,9 @@ makeGridColumns();
 blackChange();
 
 
+
 //Adding functions to buttons
  pixelSizeButton.addEventListener("click",() => setPixelSize());
  newGridButton.addEventListener('click', () => newGridChange());
+ rainbowModeButton.addEventListener('click', () => rainbowModeChange());
+ blackModeButton.addEventListener('click', () => blackChange());
